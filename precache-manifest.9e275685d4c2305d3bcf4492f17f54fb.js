8663ffdf987a2108d3438b9cfdd212cfab591a28
dbvisit <dbvisit@kiwi.localdomain>
2020-09-28 10:09:41 +1300

    Updates

---
+++ precache-manifest.9e275685d4c2305d3bcf4492f17f54fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f841f8f89f2dd39b372c9a5dc92046e3",
+    "url": "/react-monsters-app/index.html"
+  },
+  {
+    "revision": "0e404fc107389396c39c",
+    "url": "/react-monsters-app/static/css/main.661daa44.chunk.css"
+  },
+  {
+    "revision": "86dcdc79c8fa6ca96a42",
+    "url": "/react-monsters-app/static/js/2.1bbf21c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-app/static/js/2.1bbf21c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e404fc107389396c39c",
+    "url": "/react-monsters-app/static/js/main.6b330745.chunk.js"
+  },
+  {
+    "revision": "88181aecd39d84ca704b",
+    "url": "/react-monsters-app/static/js/runtime-main.d95a17f7.js"
+  }
+]);
